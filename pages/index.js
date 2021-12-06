2dbb61d2767136df93e9a55f9036e0310ec8722e
Jon Rydarowski <jrydarowski@gmail.com>
2021-12-06 15:37:38 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Test Me Test Me 1,2,3</title>
         <link rel="icon" href="/favicon.ico" />
+    <script data-jsd-embedded data-key="bca31543-214d-40ea-84ec-28764127c22d" data-base-url="https://jsd-widget.atlassian.com" src="https://jsd-widget.atlassian.com/assets/embed.js"></script>
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to the internet!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
@@ -19,7 +20,5 @@ export default function Home() {
 
       <Footer />
     </div>
-    <script data-jsd-embedded data-key="bca31543-214d-40ea-84ec-28764127c22d" data-base-url="https://jsd-widget.atlassian.com" src="https://jsd-widget.atlassian.com/assets/embed.js">
-    </script>
   )
 }
