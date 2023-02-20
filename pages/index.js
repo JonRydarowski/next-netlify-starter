fc458c709599d55635e5caa8e8e840c6763499a9
Jon Rydarowski <jrydarowski@gmail.com>
2023-02-19 21:19:54 -0500

    updated form

--- pages/index.js
+++ pages/index.js
@@ -22,43 +22,49 @@ export default function Home() {
       <main>
       <div class="container-fluid">
       <Header title="Welcome Frank - Nothing to see here." />
-      <p>Pretent that this is important copy that we want to make sure is captured in one way or the other about this that and the other.</p>
+      <p>Hello, all and welcome. Please use the form below to access this site. If it appears as though the form does not work then it's likey the case that you should not be here. Pretty ominous huh?</p>
 </div>
         <p className="description">
-          Stretch and get started today! It's a great time to get going. </p>
+          A couple additional options to try</p>
           <div class="row text-white text-center pb-3">
   <div class="col-4 bg-primary pt-3">
-    <p>Do you see me?<br />[col-4]</p>
+    <p>Intro: Hi, I'm jsI18nTransformer<br />[col-4]</p>
   </div>
   <div class="col-4 bg-dark pt-3">
-    <p>I see you! <br />[col-4]</p>
+    <p>Past work and similar <br />[col-4]</p>
   </div>
   <div class="col-4 bg-secondary pt-3">
-    <p>What? <br />[col-4]</p>
+    <p>Upcoming projects and articles <br />[col-4]</p>
   </div>
 </div>
     <form name="Contact" method="POST" data-netlify="true">
-    <p className="prom"> Pass along some info and we'll read it. </p>
+    <p className="prom">Old school access request</p>
     <input type="hidden" name="form-name" value="Contact" />
   <p>
-    <label>Your Name: <input type="text" name="name" /></label>
+    <label>Name: <input type="text" name="name" /></label>
   </p>
   <p>
-    <label>Your Email: <input type="email" name="email" /></label>
+    <label>Email: <input type="email" name="email" /></label>
   </p>
   <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
+    <label>Access: <select name="role[]" multiple>
+      <option value="family">Family</option>
+      <option value="friend">Friend</option>
+      <option value="professional">Professional</option>
+      <option value="new">Guest</option>
     </select></label>
   </p>
   <p>
-    <label>Special Message: <textarea name="message"></textarea></label>
+    <label>Anything Else: <textarea name="message"></textarea></label>
   </p>
   <p>
-    <button type="submit">Send to Jon</button>
+    <label>username <input type="text" name="username" /></label>
+  </p>
+  <p>
+    <button type="submit">Send it homeboy</button>
   </p>
 </form>
+<hr>  </hr>
     <script src="https://jsp8g2gvqw36.statuspage.io/embed/script.js"></script>
       </main>
     <p> Welcome home you crazy animal.</p>
