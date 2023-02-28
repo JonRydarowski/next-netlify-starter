13d94829f20e300cd966bcd6598d450cab4ce7b1
Jon Rydarowski <jrydarowski@gmail.com>
2023-02-28 15:31:24 +0000

    last try

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
         <div class="container-fluid">
           <div class="row flex-xl-nowrap">
           <main>
-          <nav class="navbar navbar-expand-xl navbar-light bg-light">
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -54,6 +54,10 @@ export default function Home() {
         <a class="nav-link disabled">Disabled</a>
       </li>
     </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
   </div>
 </nav>
           <div class="container-xl">
