7601748cbb74a049341bccf4bef5ecd9c10f9819
Jon Rydarowski <jrydarowski@gmail.com>
2023-02-28 15:21:04 +0000

    made xl

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
         <div class="container-fluid">
           <div class="row flex-xl-nowrap">
           <main>
-          <div class="container">
+          <div class="container-xl">
           <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
