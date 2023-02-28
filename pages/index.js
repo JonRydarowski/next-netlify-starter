d86c3351ba750459ed94a9a728cdd4528ec996a9
Jon Rydarowski <jrydarowski@gmail.com>
2023-02-28 15:42:13 +0000

    closed input

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,7 @@ export default function Home() {
         <div class="container-fluid">
           <div class="row flex-xl-nowrap">
           <main>
+          <div class="container-xl">
           <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -56,11 +57,11 @@ export default function Home() {
     </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        </input>
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
 </nav>
-          <div class="container-xl">
   <div class="row text-white text-left pb-3">
     <div class="col bg-dark pt-3">
     <Header title="Welcome Frank!"/>
