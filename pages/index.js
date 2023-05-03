accec4e293a541e4e814e255dbd5c551d0c2bcdb
Jon Rydarowski <jrydarowski@gmail.com>
2023-05-03 13:50:01 +0000

    part one of script

--- pages/index.js
+++ pages/index.js
@@ -23,38 +23,11 @@ export default function Home() {
     v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
         o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
         y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
-        z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+        z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo')
+        })('c7521485-8a07-4603-7595-9f859527411a');
+      </script>
 
-        // This function creates anonymous visitor IDs in Pendo unless you change the visitor id field to use your app's values
-        // This function uses the placeholder 'ACCOUNT-UNIQUE-ID' value for account ID unless you change the account id field to use your app's values
-        // Call this function after users are authenticated in your app and your visitor and account id values are available
-        // Please use Strings, Numbers, or Bools for value types.
-        pendo.initialize({
-            visitor: {
-                id:              'guest-first-one-rydarowski'   // Required if user is logged in, default creates anonymous ID
-                email:        'jon@rydarowski.com'
-                // full_name:    // Recommended if using Pendo Feedback
-                // role:         // Optional
-
-                // You can add any additional visitor level key-values here,
-                // as long as it's not one of the above reserved names.
-            },
-
-            account: {
-                id:           'jon-rydarowski' // Required if using Pendo Feedback, default uses the value 'ACCOUNT-UNIQUE-ID'
-                // name:         // Optional
-                // is_paying:    // Recommended if using Pendo Feedback
-                // monthly_value:// Recommended if using Pendo Feedback
-                // planLevel:    // Optional
-                // planPrice:    // Optional
-                // creationDate: // Optional
-
-                // You can add any additional account level key-values here,
-                // as long as it's not one of the above reserved names.
-            }
-        });
-})('c7521485-8a07-4603-7595-9f859527411a');
-</script>
+     
     </Head>
     <body>
       <div class="skippy overflow-hidden">
