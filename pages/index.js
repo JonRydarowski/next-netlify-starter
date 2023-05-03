ffb9099c73c3cae4df463ced058fd131572f8c74
Jon Rydarowski <jrydarowski@gmail.com>
2023-05-03 14:34:51 +0000

    added back in

--- pages/index.js
+++ pages/index.js
@@ -114,6 +114,15 @@ export default function Home() {
   </p>
 </form>
     <script src="https://jsp8g2gvqw36.statuspage.io/embed/script.js"></script>
+    <script>
+(function(apiKey){
+    (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
+    v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
+        o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
+        y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
+        z=e.getElementsByTagName(n)[0];
+        z.parentNode.insertBefore(y,z);})(window,document,'script','pendo')})('c7521485-8a07-4603-7595-9f859527411a');
+</script>
       </main>
       </div>
       </div>
