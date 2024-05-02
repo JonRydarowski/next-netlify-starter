ebaca496cfcb6e2b6c640e37546fe449456ca695
Jon Rydarowski <jrydarowski@gmail.com>
2024-05-02 15:49:29 -0400

    Update index.js
    bing

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,32 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+    <!DOCTYPE html>
+<html>
+  <head>
+    <title>Parcel Sandbox</title>
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <style>
+        body {
+            width: 100%;
+            height: 100%;
+            background-image: url('https://assets.crawco.com/photos/_fullWidth/global-innovations-hero.jpg');
+            background-repeat: no-repeat;         
+}
+    </style>
+  </head>
+  <body>
+    <div></div>
+    <!-- 1. Load the webchat.js bundle via a script tag -->
+    <script src="https://github.com/Cognigy/WebchatWidget/releases/download/v3.0.0-beta.22/webchat.js"></script>
+    <script>
+        initWebchat(
+            "https://endpoint-app-us.cognigy.ai/cec175b2e3c3e163731fed0a18a9293e1d759b8e35f26994561e9b5046091d4c"
+        );
+    </script>
+  </body>
+</html>
+   /* <div className="container">
       <Head>
         <title>Welcome home scott</title>
         <link rel="icon" href="/favicon.ico" />
@@ -120,5 +145,5 @@ export default function Home() {
       </body>
       <Footer />
     </div>
-  )
+  )*/
 }
